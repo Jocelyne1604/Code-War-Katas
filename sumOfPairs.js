0f08e91488b6db30e838db20ee2e1aad5aae85bc
joydev1604 <lyne1604@gmail.com>
2019-09-13 17:24:34 -0400

    Complete sumOfPairs.js

--- sumOfPairs.js
+++ sumOfPairs.js
@@ -1,36 +1,19 @@
 var sum_pairs = function(numbers, sum) {
-  // a place to store the pairs we encounter
-  const pairs = [];
-  // for each number in the array
-  numbers.forEach((leftNumber, leftIndex) => {
-    // iterate over the array
-    for (
-      let rightIndex = leftIndex + 1;
-      rightIndex < numbers.length;
-      rightIndex++
-    ) {
-      const rightNumber = numbers[rightIndex];
-      // add the two values
-      // if they are equal to sum
-      if (leftNumber + rightNumber == sum) {
-        // store the numbers and the right index
-        pairs.push({
-          pair: [leftNumber, rightNumber],
-          rightIndex
-        });
-      }
+  // a place to keep track of the numbers we have seen
+  const seenNumbers = {};
+  // iterate over the numbers
+  for (let i = 0; i < numbers.length; i++) {
+    const currentNumber = numbers[i];
+    // subtract current number from sum to get the number we need
+    const need = sum - currentNumber;
+    // check to see if we've seen the number we need before
+    if (seenNumbers[need]) {
+      // if we have return the pair
+      return [need, currentNumber];
+    } else {
+      // if not, set number as seen
+      seenNumbers[currentNumber] = true;
     }
-  });
-
-  if (pairs.length > 0) {
-    let earliestPair = pairs[0];
-    for (let i = 1; i < pairs.length; i++) {
-      if (earliestPair.rightIndex > pairs[i].rightIndex) {
-        earliestPair = pairs[i];
-      }
-    }
-    // return the earliest pair (lowest second index)
-    return earliestPair.pair;
   }
-  return undefined;
+  // return undefined
 };
